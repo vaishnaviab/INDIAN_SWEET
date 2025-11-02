4fab96f06123f02431aa372616f76aa098310f77
vaishnaviab <vaichirao24@gmail.com>
2025-11-02 22:52:13 +0530

    Add files via upload

---
+++ prisma.test.js
@@ -0,0 +1,9 @@
+const { PrismaClient } = require("@prisma/client");
+const db = new PrismaClient();
+db.$connect().then(() => {
+  console.log("? Prisma client connected on", process.version);
+  return db.$disconnect();
+}).catch(e => {
+  console.error("? Prisma test failed:", e);
+  process.exit(1);
+});
